0e73e7f9e2086d8667fb2e26445e5733876d931d
bcolocho <127900656+bcolocho@users.noreply.github.com>
2023-05-02 00:12:16 -0600

    ejercicios JS
    clases inicial javascriot

---
+++ JAVASCRIPT/concepto.js
@@ -0,0 +1,35 @@
+// para comentario de una sola line
+/**
+/* asignacion de variables
+*
+*let y var (palabra reservada)
+*
+* let: las variables no se pueden utilizar su nombre mas de una vez y le podemos asignar un dato o no
+* se trabaja de manera local
+*
+* var: las variables se pueden sobre declarar y podemos asignarle un dato o no
+* se trabaja de manera global
+*/
+
+//declarando variables
+let nombre = "maria";
+let variable;
+
+var apellido;
+//estamos sobre dclaramos la variable
+var apellido ="hernandez";
+
+//llamando variables
+nombre; //maria
+nombre = "juana";
+nombre; //juana
+apellido;
+
+//definiendo una constante const
+const PI = 3.1416; //varlor estatico
+const num = 25;
+
+//imprimiendo en javascript por medio de la consola
+console.log(PI);
+
+
